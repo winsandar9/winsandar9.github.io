5f0310a1ef7f3789bc83aa083fabdc4565a6ba0d
winsandar9 <missbrowny1995@gmail.com>
2025-10-02 08:04:59 +0630

    mycsspro

---
+++ Javascript/js/logical.js
@@ -0,0 +1,14 @@
+// && and
+// at least one false return false
+console.log(23 < 25 && 30 < 30);
+
+// || or
+// at least one true return true
+console.log(23 < 25 || 30 < 30);
+
+// ! not
+
+let num = 1;
+ if (!num) {
+    console.log("Not Operator");
+ }
